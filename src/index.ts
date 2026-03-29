c7c380b4d553695841ea07b7b7547d0758039c83
ExcelDsigN-tech <oluwadamilareoduyomi.com>
2026-03-29 07:50:49 +0100

    feat(transactions): implement monitorTransaction polling loop

--- src/index.ts
+++ src/index.ts
@@ -30,5 +30,5 @@ export type { SubmitResult, TransactionStatus } from './types/transaction';
 
 // 6. Standalone functions
 export { createEscrowAccount, lockCustodyFunds, anchorTrustHash, verifyEventHash } from './escrow';
-export { buildMultisigTransaction } from './transactions';
+export { buildMultisigTransaction, fetchTransactionOnce, monitorTransaction } from './transactions';
 export { getMinimumReserve } from './accounts';
