44d4430e9fdd1acc876ec19f7b0693707c4fcb05
Onelove <onwe.manasseh@gmail.com>
2026-04-02 07:38:33 +0100

    feat(sdk): implement submitTransaction with error handling #119

--- src/sdk.ts
+++ src/sdk.ts
@@ -1,1 +1,9 @@
-export class StellarSDK {}
+import { Horizon } from '@stellar/stellar-sdk';
+import { TESTNET_HORIZON_URL } from './utils/constants';
+import { SDKConfig } from './types/network';
+
+export const horizonClient = new Horizon.Server(TESTNET_HORIZON_URL);
+
+export class StellarSDK {
+  constructor(public config: SDKConfig) {}
+}
